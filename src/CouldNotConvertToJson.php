5ac33004c29c07d7cb90adf8418d788bfb117fe7
Stratadox <jesse@stratadox.com>
2018-02-11 21:42:15 +0100

    Throw a conversion failed exception on unserializable input

--- src/CouldNotConvertToJson.php
+++ src/CouldNotConvertToJson.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace Stratadox\ImmutableCollection;
 
+use function get_class as theClassOf;
 use RuntimeException;
-use function sprintf;
+use function sprintf as withMessage;
+use Stratadox\Collection\Collection;
 use Stratadox\Collection\ConversionFailed;
 use Throwable;
 
 final class CouldNotConvertToJson extends RuntimeException implements ConversionFailed
 {
-    public static function encountered(Throwable $exception) : self
+    public static function encountered(
+        Collection $theCollection,
+        Throwable $exception
+    ) : ConversionFailed
     {
-        return new self(sprintf('Could not '));
+        return new CouldNotConvertToJson(withMessage(
+            'Could not convert the `%s` class to json: %s',
+            theClassOf($theCollection),
+            $exception->getMessage()
+        ), 0, $exception);
     }
 }
--- src/JsonSerializing.php
+++ src/JsonSerializing.php
@@ -4,17 +4,26 @@ declare(strict_types=1);
 
 namespace Stratadox\ImmutableCollection;
 
+use Stratadox\Collection\Collection;
+use Stratadox\Collection\ConversionFailed;
 use Stratadox\Collection\JsonSerializable;
+use Throwable;
 
 trait JsonSerializing
 {
     /**
      * @see JsonSerializable::json()
      * @return string
+     * @throws ConversionFailed
      */
     public function json() : string
     {
-        return json_encode($this->items());
+        try {
+            return json_encode($this->items());
+        } catch (Throwable $exception) {
+            /** @var Collection $this */
+            throw CouldNotConvertToJson::encountered($this, $exception);
+        }
     }
 
     /** @see Collection::items() */
