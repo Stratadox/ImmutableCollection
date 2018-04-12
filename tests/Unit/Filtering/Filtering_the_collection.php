0ad56dc41b4a0753f72966e6fe6a5699b5b4308c
Stratadox <info@stratadox.com>
2018-04-13 00:09:16 +0200

    Add tests for filtering through a closure

--- tests/Unit/Filtering/Filtering_the_collection.php
+++ tests/Unit/Filtering/Filtering_the_collection.php
@@ -42,7 +42,7 @@ class Filtering_the_collection extends TestCase
 
     /**
      * @dataProvider startingWith
-     * @scenario
+     * @test
      */
     function filtering_by_HaveANameStartingWith_specification(
         string $filterText,
@@ -60,18 +60,46 @@ class Filtering_the_collection extends TestCase
         );
     }
 
+    /** @test */
+    function filtering_with_a_function()
+    {
+        $numbers = new Numbers(1, 2, 3, 4, 5);
+
+        $this->assertEquals(
+            new Numbers(3, 4, 5),
+            $numbers->filterWith(function (int $number) {
+                return $number > 2;
+            })
+        );
+    }
+
+    /** @test */
+    function filtering_with_a_function_preserves_my_boolean_flag()
+    {
+        $numbers = new FlaggedNumbers(true, 1, 2, 3, 4, 5);
+
+        $numbers = $numbers->filterWith(function (): bool {
+            return true;
+        });
+
+        $this->assertTrue($numbers->flagged());
+    }
+
     public function startingWith() : array
     {
         return [
-            'Starting with "h"' => ['h',
+            'Starting with "h"' => [
+                'h',
                 new FilterableThings()
             ],
-            'Starting with "o"' => ['o',
+            'Starting with "o"' => [
+                'o',
                 new FilterableThings(
                     new Thing('one')
                 )
             ],
-            'Starting with "t"' => ['t',
+            'Starting with "t"' => [
+                't',
                 new FilterableThings(
                     new Thing('two'),
                     new Thing('three')
