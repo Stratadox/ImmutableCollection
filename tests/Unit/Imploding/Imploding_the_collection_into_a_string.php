cdaee15b81e232d8b4f90a7929f92a1ac6f9c731
Stratadox <info@stratadox.com>
2018-04-14 20:48:57 +0200

    Add test for getting an exception when unable to implode

--- tests/Unit/Imploding/Imploding_the_collection_into_a_string.php
+++ tests/Unit/Imploding/Imploding_the_collection_into_a_string.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Stratadox\ImmutableCollection\Test\Unit\JsonSerializing;
 
 use PHPUnit\Framework\TestCase;
+use Stratadox\Collection\ConversionFailed;
+use Stratadox\ImmutableCollection\Test\Unit\Imploding\Things\NotStringConvertibleThing;
 use Stratadox\ImmutableCollection\Test\Unit\Imploding\Things\StringConvertibleThing;
 use Stratadox\ImmutableCollection\Test\Unit\Imploding\Things\Things;
 
 /**
  * @covers \Stratadox\ImmutableCollection\Imploding
  * @covers \Stratadox\ImmutableCollection\ImmutableCollection
+ * @covers \Stratadox\ImmutableCollection\CouldNotImplode
  */
 class Imploding_the_collection_into_a_string extends TestCase
 {
@@ -36,5 +39,24 @@ class Imploding_the_collection_into_a_string extends TestCase
         );
 
         $this->assertEquals('foo; bar; baz', $things->implode('; '));
+    }
+
+    /** @test */
+    function getting_and_exception_when_trying_to_implode_the_unimplodable()
+    {
+        $things = new Things(
+            StringConvertibleThing::named('foo'),
+            StringConvertibleThing::named('bar'),
+            NotStringConvertibleThing::named('baz')
+        );
+
+        $this->expectException(ConversionFailed::class);
+        $this->expectExceptionMessage(
+            'Could not implode the `'.Things::class.'` class: ' .
+            'Object of class ' . NotStringConvertibleThing::class . ' could ' .
+            'not be converted to string'
+        );
+
+        $things->implode('; ');
     }
 }
